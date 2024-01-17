e5ddd1776820c049fa21d5530ebb8e2e0f2f753e
hovering-clouds <80081529+hovering-clouds@users.noreply.github.com>
2024-01-17 17:56:15 +0800

    Site updated: 2024-01-17 17:56:14

--- js/load.js
+++ js/load.js
@@ -1,4 +1,3 @@
-//var resourcesPath = 'https://cdn.jsdelivr.net/gh/hovering-clouds/blog-assets@v1.7/'; // 指定资源文件（模型）保存的路径，使用github的release版本，路径如下https://cdn.jsdelivr.net/gh/用户/库@版本号/资源路径
-var resourcesPath = '/blog/model/'; // 指定资源文件（模型）保存的路径，使用github的release版本，路径如下https://cdn.jsdelivr.net/gh/用户/库@版本号/资源路径
+var resourcesPath = 'https://cdn.jsdelivr.net/gh/hovering-clouds/blog-assets@v1.7/'; // 指定资源文件（模型）保存的路径，使用github的release版本，路径如下https://cdn.jsdelivr.net/gh/用户/库@版本号/资源路径
 var modelDir = ['pony']; // 指定需要加载的模型
 window.initPath(resourcesPath, modelDir); // 初始化模型
